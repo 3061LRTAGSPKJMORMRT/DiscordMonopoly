38a3bcf7ecf4ef57e40caa33a8e21250d5078857
3061LRTAGSPKJMORMRT <53869236+3061LRTAGSPKJMORMRT@users.noreply.github.com>
2021-02-13 14:34:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const Discord = require("discord.js")
+const db = require("quick.db")
+const client = new Discord.Client()
+const prefix = "m!"
+
+
+client.on("ready", async () => {
+    console.log("Bot is ready!")
+    client.user.setActivity("Discordopoly", {type: "COMPETING"})
+})
+
+client.on("message", async message => {
+    if (message.author.bot || !message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const cmd = args.shift().toLowerCase()
+})
